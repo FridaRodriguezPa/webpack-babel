a533a196e82631ecd9b15bce591b404aee435e84
Frida Rodriguez <fridaixchelrp@gmail.com>
2021-10-04 19:15:20 -0500

    Ejercicios 04 - Sep

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { suma,saludo } from './operaciones';
+
+//document.write(`<h2>La suma ${suma}</h2>`);
+
+console.log(suma(2,3));
+console.log(saludo);
---
+++ src/operaciones.js
@@ -0,0 +1,12 @@
+// exportar la funcion
+export const suma = (num, num2) => {
+    return num + num2;
+}
+const resta = (num, num2) => {
+    return num - num2;
+}
+const multi = (num, num2) => {
+    return num * num2;
+}
+
+export const saludo = 'Hola, Soy Frida, Saludos desde un Módulo';
